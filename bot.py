e5b70ac42926e2fb129fb8f9ce316aae84840e08
Christopher Meyers <Joe.Master@att.net>
2014-11-04 18:43:28 -0800

    proper support for the standard settings file

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import re
 import settings
 
 r = praw.Reddit(user_agent='r/starcraft new player bot')
-r.login(reddituser, redditpass)
+r.login(settings.reddituser, settings.redditpass)
 subreddit = r.get_subreddit('jo3m3tal')
 comments = praw.helpers.comment_stream(r,subreddit)
 
